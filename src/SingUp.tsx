52f5964a3faa1ab6cfd8b157a7cde94e47741d93
Sofia Bargues <barguessofia@gmail.com>
2024-03-25 16:47:27 +0000

    ert

--- src/SingUp.tsx
+++ src/SingUp.tsx
@@ -42,17 +42,17 @@ function SingUp() {
     return (
       <div>
         {" "}
-        <div className="h-screen lg:bg-[rgb(54,56,76)] lg:h-screen lg:p-10 lg:items-center lg:justify-center  lg:grid">
-          <div className="m-6 lg:m-0 lg:h-[520px] flex flex-col justify-center  lg:flex-col lg:p-20 lg:rounded-2xl lg:bg-white lg:max-w-[928px] ">
-            <div className=" py-6 lg:my-10 lg:flex lg:flex-col lg:justify-around">
+        <div className="h-screen  sm:bg-[rgb(54,56,76)] sm:h-screen sm:p-10 sm:items-center sm:justify-center  sm:grid">
+          <div className="m-6 sm:m-0 sm:h-[520px] flex flex-col justify-center  sm:flex-col sm:p-20 sm:rounded-2xl sm:bg-white sm:max-w-[928px] ">
+            <div className=" py-6 sm:my-10 sm:flex sm:flex-col sm:justify-around">
               <div className="flex flex-col gap-9">
                 <div className="">
-                  <img src={iconList} className="lg:mt-20 h-[64px]" />
+                  <img src={iconList} className="sm:mt-20 h-[64px]" />
                 </div>
-                <h1 className="font-bold text-3xl text-[#242742] lg:text-5xl ">
+                <h1 className="font-bold text-3xl text-[#242742] sm:text-5xl ">
                   Thanks for<br></br> subscribing!
                 </h1>
-                <p className="my-5  lg:my-0 w-[376px] h-[96px] lg:h-[72px] lg:text-left lg:text-sm text-[#242742]">
+                <p className="my-5  sm:my-0 w-[376px] h-[96px] sm:h-[72px] sm:text-left sm:text-sm text-[#242742]">
                   A confirmation email has been sent to{" "}
                   <span className="font-bold">{currentEmailValue()}</span>.
                   Please open it and click the button inside to confirm your
@@ -61,11 +61,11 @@ function SingUp() {
                 <form
                   action="email"
                   method="GET"
-                  className="flex flex-col justify-end lg:justify-normal h-[150px]"
+                  className="flex flex-col justify-end sm:justify-normal h-[150px]"
                 >
                   <button
                     onClick={swichToSingup}
-                    className="hover:bg-[#ff6742] hover:shadow-lg hover:shadow-[#ffa8a2] hover:bg-gradient-to-b from-[#FF6A3A] to-[#FF527B] bg-blue-950 text-sm lg:text-xl lg:rounded-2xl lg:w-[376px] lg:h-[56px] font-semibold text-white w-full h-11 rounded-md"
+                    className="hover:bg-[#ff6742] hover:shadow-sm hover:shadow-[#ffa8a2] hover:bg-gradient-to-b from-[#FF6A3A] to-[#FF527B] bg-blue-950 text-sm sm:text-xl sm:rounded-2xl sm:w-[376px] sm:h-[56px] font-semibold text-white w-full h-11 rounded-md"
                     type="submit"
                   >
                     Dismiss message
@@ -81,39 +81,39 @@ function SingUp() {
 
   const singupState = () => {
     return (
-      <div className=" lg:bg-[#36384c] lg:h-screen  lg:p-10 lg:items-center lg:justify-center  lg:grid">
-        <div className=" flex flex-col lg:flex-row-reverse lg:p-4 lg:rounded-2xl lg:bg-white lg:max-w-[928px] ">
-          <div className="block lg:hidden">
+      <div className=" sm:bg-[#36384c] sm:h-screen  sm:p-10 sm:items-center sm:justify-center  sm:grid">
+        <div className=" flex items-center flex-col sm:flex-row-reverse sm:p-4 sm:rounded-2xl sm:bg-white sm:max-w-[928px] ">
+          <div className="block self-center sm:hidden">
             <img src={mobile} />
           </div>
-          <div className="hidden lg:block">
+          <div className="hidden sm:block self-center ">
             <img src={desktop} />
           </div>
-          <div className="px-5  py-10 lg:mx-10 lg:flex lg:flex-col lg:justify-around">
-            <h1 className="font-bold text-3xl text-blue-950  lg:text-6xl">
+          <div className="px-5  py-10 sm:mx-10 sm:flex sm:flex-col sm:justify-around">
+            <h1 className=" font-bold text-3xl text-blue-950  sm:text-6xl">
               {" "}
               Stay updated!{" "}
             </h1>
-            <p className="my-5 text-sm lg:text-lg">
+            <p className="my-5 text-sm sm:text-sm">
               Join 60,000+ product managers receiving monthly updates on:
             </p>
             <ul className="">
-              <li className="flex lg:items-center">
+              <li className="flex sm:items-center">
                 <img src={iconList} className=" h-4" />
-                <p className="mx-3   text-sm lg:text-lg">
+                <p className="mx-3   text-sm sm:text-sm">
                   Product discovery and building what matters
                 </p>
               </li>
-              <li className="flex lg:items-center">
+              <li className="flex sm:items-center">
                 <img src={iconList} className=" h-4" alt="React logo" />
-                <p className="mx-3  text-sm lg:text-lg">
+                <p className="mx-3  text-sm sm:text-sm">
                   {" "}
                   Measuring to ensure updates are a success
                 </p>
               </li>
-              <li className="flex lg:items-center">
+              <li className="flex sm:items-center">
                 <img src={iconList} className=" h-4" alt="React logo" />
-                <p className="mx-3  text-sm lg:text-lg"> And much more!</p>
+                <p className="mx-3  text-sm sm:text-sm"> And much more!</p>
               </li>
             </ul>
             <form action="email" method="GET" className="flex flex-col mt-7">
@@ -130,7 +130,7 @@ function SingUp() {
                   type="email"
                   ref={inputRefEmail}
                   placeholder="email@company.com"
-                  className="h-11 border rounded-md lg:h-[58px] w-full my-3 px-5  "
+                  className="h-11 border rounded-md sm:h-[58px] w-full my-3 px-5  "
                   style={{
                     borderColor: isActiveError ? "hsl(4, 100%, 67%)" : "",
                     backgroundColor: isActiveError
@@ -142,7 +142,7 @@ function SingUp() {
 
                 <button
                   onClick={submitEmail}
-                  className="hover:bg-[#ff6742] lg:h-[58px] hover:shadow-lg hover:shadow-[#ffa8a2] hover:bg-gradient-to-b from-[#FF6A3A] to-[#FF527B] bg-blue-950 text-sm  font-semibold text-white  w-full  h-11 rounded-md"
+                  className="hover:bg-[#ff6742] sm:h-[58px] hover:shadow-sm hover:shadow-[#ffa8a2] hover:bg-gradient-to-b from-[#FF6A3A] to-[#FF527B] bg-blue-950 text-sm  font-semibold text-white  w-full  h-11 rounded-md"
                   type="submit"
                 >
                   Subscribe to monthly newsletter
