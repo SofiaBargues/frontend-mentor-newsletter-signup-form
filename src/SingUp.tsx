48223935b0b5d22eccbadaa0442f7715efd7f8d2
Sofia Bargues <barguessofia@gmail.com>
2024-03-25 15:07:32 +0000

    dfg

--- src/SingUp.tsx
+++ src/SingUp.tsx
@@ -5,12 +5,12 @@ import RobotoB from "../assets/fonts/Roboto-Bold.ttf";
 import iconList from "../assets/images/icon-list.svg";
 function SingUp() {
   const [errorEmail, setErrorEmail] = useState("");
-  const [isActivrError, setIsActivrError] = useState(false);
+  const [isActiveError, setIsActiveError] = useState(false);
   const [showState, setShowState] = useState(false);
   const inputRefEmail = useRef(null);
 
   function isValidEmail(email) {
-    return /\S+@S+\.\S+/.test(email);
+    return /\S+@\S+\.\S+/.test(email);
   }
 
   const swichToSingup = () => {
@@ -23,16 +23,17 @@ function SingUp() {
 
   const submitEmail = (event) => {
     event.preventDefault();
+    console.log(inputRefEmail.current.value);
     if (
       inputRefEmail.current.value === "" ||
       !isValidEmail(inputRefEmail.current.value)
     ) {
       setErrorEmail("Valid email required");
-      setIsActivrError(true);
+      setIsActiveError(true);
       setShowState(false);
     } else {
       setErrorEmail("");
-      setIsActivrError(false);
+      setIsActiveError(false);
       setShowState(true);
     }
   };
@@ -53,7 +54,7 @@ function SingUp() {
                 </h1>
                 <p className="my-5  lg:my-0 w-[376px] h-[96px] lg:h-[72px] lg:text-left lg:text-sm text-[#242742]">
                   A confirmation email has been sent to{" "}
-                  <span className="font-bold">ash@loremcompany.com</span>.
+                  <span className="font-bold">{currentEmailValue()}</span>.
                   Please open it and click the button inside to confirm your
                   subscription.
                 </p>
@@ -63,6 +64,7 @@ function SingUp() {
                   className="flex flex-col justify-end lg:justify-normal h-[150px]"
                 >
                   <button
+                    onClick={swichToSingup}
                     className="hover:bg-[#ff6742] hover:shadow-lg hover:shadow-[#ffa8a2] hover:bg-gradient-to-b from-[#FF6A3A] to-[#FF527B] bg-blue-950 text-sm lg:text-xl lg:rounded-2xl lg:w-[376px] lg:h-[56px] font-semibold text-white w-full h-11 rounded-md"
                     type="submit"
                   >
@@ -115,17 +117,35 @@ function SingUp() {
               </li>
             </ul>
             <form action="email" method="GET" className="flex flex-col mt-7">
-              <label className="text-xs font-extrabold  text-blue-950  ">
-                Email address
-              </label>
+              <div className="label-box">
+                <label
+                  ref={inputRefEmail}
+                  className="text-xs font-extrabold  text-blue-950  "
+                >
+                  Email address
+                </label>
+                {/* <label
+                  ref={errorEmail}
+                  className="text-xs font-extrabold  text-blue-950  "
+                ></label> */}
+              </div>
               <div className="">
                 <input
                   type="email"
+                  ref={inputRefEmail}
                   placeholder="email@company.com"
                   className="h-11 border rounded-md lg:h-[58px] w-full my-3 px-5  "
+                  style={{
+                    borderColor: isActiveError ? "hsl(4, 100%, 67%)" : "",
+                    backgroundColor: isActiveError
+                      ? "hsl(4, 100%, 67%,0.2)"
+                      : "",
+                    color: isActiveError ? "hsl(4, 100%, 67%)" : "",
+                  }}
                 ></input>
 
                 <button
+                  onClick={submitEmail}
                   className="hover:bg-[#ff6742] lg:h-[58px] hover:shadow-lg hover:shadow-[#ffa8a2] hover:bg-gradient-to-b from-[#FF6A3A] to-[#FF527B] bg-blue-950 text-sm  font-semibold text-white  w-full  h-11 rounded-md"
                   type="submit"
                 >
